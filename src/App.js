e391b6be216d0b2eaefaa43363df663b8b2b863d
Laura Rezende <laura.tracierra@gmail.com>
2023-12-01 21:11:06 -0300

    feat: create initial routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import UserContext from "./Context/UserContext";
+import CartContext from "./Context/CartContext";
+import { useState } from "react";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import Car from "./Pages/Car";
+import Profile from "./Pages/Profile";
 
 function App() {
+  const [user, setUser] = useState();
+  const [cart, setCart] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider
+      value={{
+        user: user || JSON.parse(localStorage.getItem("user")),
+        setUser,
+      }}
+    >
+      <CartContext.Provider
+        value={{
+          cart: cart || JSON.parse(localStorage.getItem("cart")),
+          setCart,
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/login" Component={Login} />
+            <Route path="/sign-up" Component={SignUp} />
+            <Route path="/car/:carId"  Component={Car} />
+            <Route path="/profile" Component={Profile} />
+          </Routes>
+        </BrowserRouter>
+      </CartContext.Provider>
+    </UserContext.Provider>
   );
 }
 
